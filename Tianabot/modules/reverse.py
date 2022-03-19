3f93ce558f6233cd6c4d3f5f0baa5df7b5f1c576
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-19 20:41:21 +0530

    Update reverse.py

--- Tianabot/modules/reverse.py
+++ Tianabot/modules/reverse.py
@@ -11,10 +11,10 @@ from telegram import InputMediaPhoto, TelegramError
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from Flare_Robot import dispatcher
+from Tianabot import dispatcher
 
-from Flare_Robot.modules.disable import DisableAbleCommandHandler
-from Flare_Robot.modules.helper_funcs.alternate import typing_action
+from Tianabot.modules.disable import DisableAbleCommandHandler
+from Tianabot.modules.helper_funcs.alternate import typing_action
 
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
