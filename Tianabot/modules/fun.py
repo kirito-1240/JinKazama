ad05ce4b27ab3b26b1056c0ed74411561708887d
Kirito <96290899+kirito-1240@users.noreply.github.com>
2022-03-24 19:03:46 +0530

    Update fun.py

--- Tianabot/modules/fun.py
+++ Tianabot/modules/fun.py
@@ -210,18 +210,6 @@ def hflirt(update, context):
         else update.effective_message.reply_text
     )
     reply_text(random.choice(fun.HFLIRT_STRINGS)
-               
-
-@run_async
-@typing_action
-def shrug(update, context):
-    # reply to correct message
-    reply_text = (
-        update.effective_message.reply_to_message.reply_text
-        if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
-    )
-    reply_text(random.choice(fun.SHGS))
 
 
 @run_async
