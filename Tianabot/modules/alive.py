8922e39d116a4696888c5c32eb1e5b88916c662b
Kirito <96290899+kirito-1240@users.noreply.github.com>
2022-03-28 20:24:15 +0530

    Update alive.py

--- Tianabot/modules/alive.py
+++ Tianabot/modules/alive.py
@@ -20,5 +20,5 @@ async def awake(event):
   TEXT += f"★ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"★ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/JinKazamaXBot?start=help"), Button.url("Support", "https://t.me/JinKazamaXSupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/JinKazamaXBot?start=help"), Button.url("Support", "https://t.me/JinKazama_Support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
